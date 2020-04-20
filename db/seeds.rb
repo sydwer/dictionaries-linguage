05894022a099e13007e3e17066d941d1b4b91997
Sydney Werbach <sydwer@gmail.com>
2020-04-19 19:15:12 -0600

    Added in remaining 'basic' dictionaries, and relevant loops to create them as database entries

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,20 @@ verbs = ["Able","Eat", "Enjoy", "Die", "Give", "Have", "Say",
 "Copula", "Want","Will",
 ];
 
-tenses =["Past","Present","Future", "Imperfect", "Conditional"
+tenses =["Past","Present", "Future", "Imperfect", "Conditional"
 ];
 
-determiners = ["The", "A","This", "That"
+determiners = ["The", "A", "This", "That"
 ,]
+
+prepositions = ["Around", "By", "From", "To", "Through", "Toward"
+];
+
+pronouns = ["I", "You","(Formal) You", "He", "She", "It", "We", "You (Plural)",
+"Them", "Them (Masculine)", "Them (Feminine)", "Them (Neutral)",]
+
+prepositions = ["By", "Through", "Along", "For", "Against",
+"Towards", "Without","About","From",];
 
 # bear = Noun.create(
 #     english: "Bear",
@@ -30,5 +39,33 @@ verbs.map{
     |verb| Word.create(
         english: verb,
         category: "verb",
+    )
+}
+
+tenses.map{
+    |tense| Word.create(
+        english: tense,
+        category: "tense",
+    )
+}
+
+determiners.map{
+    |determiner| Word.create(
+        english: determiner,
+        category: "determiner",
+    )
+}
+
+pronouns.map{
+    |pronoun| Word.create(
+        english: pronoun,
+        category: "pronoun",
+    )
+}
+
+prepositions.map{
+    |preposition| Word.create(
+        english: preposition,
+        category: "preposition",
     )
 }
