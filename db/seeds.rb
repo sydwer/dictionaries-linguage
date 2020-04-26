2b4e2bc0e613a04b2d0166eb839174f4e90493ac
Sydney Werbach <sydwer@gmail.com>
2020-04-25 22:19:18 -0600

    Added in seeds for 'Dictionary' that include case options

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,20 @@
 Word.destroy_all
 Language.destroy_all
 
-nouns = ["Apple","Bear","Book", "Cat", "Dinner", "Fish",
+nouns_words = ["Apple","Bear","Book", "Cat", "Dinner", "Fish",
 "Hello", "House","Hunger", "River","Rock", "Ten","Tree"];
 
-pronouns = ["I", "You","(Formal) You", "He", "She", "It", "We", "You (Plural)",
+pronouns_words = ["I", "You","(Formal) You", "He", "She", "It", "We", "You (Plural)",
 "Them", "Them (Masculine)", "Them (Feminine)", "Them (Neutral)"]
 
-verbs = ["Able","Eat", "Enjoy", "Die", "Give", "Have", "Say",
+verbs_words = ["Able","Eat", "Enjoy", "Die", "Give", "Have", "Say",
 "Copula", "Want","Will"];
 
-tenses =["Past","Present", "Future", "Imperfect", "Conditional"];
+tenses_words =["Past","Present", "Future", "Imperfect", "Conditional"];
 
-determiners = ["The", "A", "This", "That"];
+determiners_words = ["The", "A", "This", "That"];
 
-prepositions = ["By", "Through", "Along", "For", "Against",
+prepositions_words = ["By", "Through", "Along", "For", "Against",
 "Towards", "Without","About","From"];
 
 grammar = [nouns, pronouns, verbs, tenses, determiners, prepositions];
@@ -24,46 +24,47 @@ languages = ["English", "German", "Japanese"];
 
 
 
+
 # make all of these just 'word' but then filter for route with category, or make join table?
 
 
 
-nouns.map{
+nouns_words.map{
     |noun| Word.create(
         english: noun,
         category: "noun",
     )
 }
 
-verbs.map{
+verbs_words.map{
     |verb| Word.create(
         english: verb,
         category: "verb",
     )
 }
 
-tenses.map{
+tenses_words.map{
     |tense| Word.create(
         english: tense,
         category: "tense",
     )
 }
 
-determiners.map{
+determiners_words.map{
     |determiner| Word.create(
         english: determiner,
         category: "determiner",
     )
 }
 
-pronouns.map{
+pronouns_words.map{
     |pronoun| Word.create(
         english: pronoun,
         category: "pronoun",
     )
 }
 
-prepositions.map{
+prepositions_words.map{
     |preposition| Word.create(
         english: preposition,
         category: "preposition",
@@ -76,3 +77,75 @@ languages.map{
         name: language
     )
 }
+
+nouns = Dictionary.create (
+    part_of_speech: "noun"
+)
+
+verbs = Dictionary.create (
+    part_of_speech: "verb"
+)
+
+tenses = Dictionary.create (
+    part_of_speech: "tense"
+)
+
+caseless_pronouns = Dictionary.create (
+    part_of_speech: "caseless pronoun"
+)
+
+caseless_determiners = Dictionary.create (
+    part_of_speech: "caseless determiner"
+)
+
+caseless_prepositions = Dictionary.create (
+    part_of_speech: "caseless preposition"
+)
+
+nominative_pronouns = Dictionary.create (
+    part_of_speech: "nominative pronoun"
+)
+
+nominative_determiners = Dictionary.create (
+    part_of_speech: "nominative determiner"
+)
+
+nominative_prepositions = Dictionary.create (
+    part_of_speech: "nominative preposition"
+)
+
+accusative_pronouns = Dictionary.create (
+    part_of_speech: "accusative pronoun"
+)
+
+accusative_determiners = Dictionary.create (
+    part_of_speech: "accusative determiner"
+)
+
+accusative_prepositions = Dictionary.create (
+    part_of_speech: "accusative preposition"
+)
+
+dative_pronouns = Dictionary.create (
+    part_of_speech: "dative pronoun"
+)
+
+dative_determiners = Dictionary.create (
+    part_of_speech: "dative determiner"
+)
+
+dative_prepositions = Dictionary.create (
+    part_of_speech: "dative preposition"
+)
+
+genitive_pronouns = Dictionary.create (
+    part_of_speech: "genitive pronoun"
+)
+
+genitive_determiners = Dictionary.create (
+    part_of_speech: "genitive determiner"
+)
+
+genitive_prepositions = Dictionary.create (
+    part_of_speech: "genitive preposition"
+)
