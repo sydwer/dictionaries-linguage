3f7dfa0369bff69879349e98d81aae8d8367c1a1
Sydney Werbach <sydwer@gmail.com>
2020-05-03 21:06:52 -0600

    Added in seeds for dictionary_entry join table for nouns, this exposed issue with creating the nouns dictionaries- current;y being fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,7 @@
 Word.destroy_all
 Language.destroy_all
+Dictionary.destroy_all
+DictionaryEntry.destroy_all
 
 nouns_words = ["Apple","Bear","Book", "Cat", "Dinner", "Fish",
 "Hello", "House","Hunger", "River","Rock", "Ten","Tree"];
@@ -19,7 +21,7 @@ prepositions_words = ["By", "Through", "Along", "For", "Against",
 
 particles_words = ["Topic","Subject", "Object", "Also", "Toward", "From", "By", "At"]
 
-grammar = [nouns, pronouns, verbs, tenses, determiners, prepositions];
+# grammar = [nouns, pronouns, verbs, tenses, determiners, prepositions];
 
 languages = ["English", "German", "Japanese"];
 
@@ -74,8 +76,10 @@ prepositions_words.map{
 }
 
 particles_words.map{
-    english: particle,
-    category: "particle",
+    |particle| Word.create(
+        english: particle,
+        category: "particle",
+    )
 }
 
 
@@ -85,101 +89,102 @@ languages.map{
     )
 }
 
-nouns = Dictionary.create (
-    part_of_speech: "noun"
+nouns = Dictionary.create(
+    :part_of_speech => "noun"
 )
 
-verbs = Dictionary.create (
-    part_of_speech: "verb"
+verbs = Dictionary.create(
+    :part_of_speech => "verb"
 )
 
-tenses = Dictionary.create (
+tenses = Dictionary.create(
     part_of_speech: "tense"
 )
 
-caseless_pronouns = Dictionary.create (
+caseless_pronouns = Dictionary.create(
     part_of_speech: "caseless pronoun"
 )
 
-caseless_determiners = Dictionary.create (
+caseless_determiners = Dictionary.create(
     part_of_speech: "caseless determiner"
 )
 
-caseless_prepositions = Dictionary.create (
+caseless_prepositions = Dictionary.create(
     part_of_speech: "caseless preposition"
 )
 
-nominative_pronouns = Dictionary.create (
+nominative_pronouns = Dictionary.create(
     part_of_speech: "nominative pronoun"
 )
 
-nominative_determiners = Dictionary.create (
+nominative_determiners = Dictionary.create(
     part_of_speech: "nominative determiner"
 )
 
-nominative_prepositions = Dictionary.create (
+nominative_prepositions = Dictionary.create(
     part_of_speech: "nominative preposition"
 )
 
-accusative_pronouns = Dictionary.create (
+accusative_pronouns = Dictionary.create(
     part_of_speech: "accusative pronoun"
 )
 
-accusative_determiners = Dictionary.create (
+accusative_determiners = Dictionary.create(
     part_of_speech: "accusative determiner"
 )
 
-accusative_prepositions = Dictionary.create (
+accusative_prepositions = Dictionary.create(
     part_of_speech: "accusative preposition"
 )
 
-dative_pronouns = Dictionary.create (
+dative_pronouns = Dictionary.create(
     part_of_speech: "dative pronoun"
 )
 
-dative_determiners = Dictionary.create (
+dative_determiners = Dictionary.create(
     part_of_speech: "dative determiner"
 )
 
-dative_prepositions = Dictionary.create (
+dative_prepositions = Dictionary.create(
     part_of_speech: "dative preposition"
 )
 
-genitive_pronouns = Dictionary.create (
+genitive_pronouns = Dictionary.create(
     part_of_speech: "genitive pronoun"
 )
 
-genitive_determiners = Dictionary.create (
+genitive_determiners = Dictionary.create(
     part_of_speech: "genitive determiner"
 )
 
-genitive_prepositions = Dictionary.create (
+genitive_prepositions = Dictionary.create(
     part_of_speech: "genitive preposition"
 )
 
-nominative_suffixes = Dictionary.create (
+nominative_suffixes = Dictionary.create(
     part_of_speech: "nominative suffix"
 )
 
-accusative_suffixes = Dictionary.create (
+accusative_suffixes = Dictionary.create(
     part_of_speech: "accusative suffix"
 )
 
-dative_suffixes = Dictionary.create (
+dative_suffixes = Dictionary.create(
     part_of_speech: "dative suffix"
 )
 
-genitive_suffixes = Dictionary.create (
+genitive_suffixes = Dictionary.create(
     part_of_speech: "genitive suffix"
 )
 
-particles = Dictionary.create (
+particles = Dictionary.create(
     part_of_speech: "particles"
 )
 
-def find(array,string){
-    array.select { |entry| entry.english == string }
-}
+def find(array,string)
+    target = array.find{ |entry| entry.english == string }
+    return target
+end
 
 apple = DictionaryEntry.create(
     word_id: find(noun_words, "Apple"),
@@ -189,3 +194,60 @@ bear = DictionaryEntry.create(
     word_id: find(noun_words, "Bear"),
     dictionary_id: nouns
 )
+
+book = DictionaryEntry.create(
+    word_id: find(noun_words, "Book"),
+    dictionary_id: nouns
+)
+
+cat = DictionaryEntry.create(
+    word_id: find(noun_words, "Cat"),
+    dictionary_id: nouns
+)
+
+dinner = DictionaryEntry.create(
+    word_id: find(noun_words, "Dinner"),
+    dictionary_id: nouns
+)
+
+fish = DictionaryEntry.create(
+    word_id: find(noun_words, "Fish"),
+    dictionary_id: nouns
+)
+
+hello = DictionaryEntry.create(
+    word_id: find(noun_words, "Hello"),
+    dictionary_id: nouns
+)
+
+house = DictionaryEntry.create(
+    word_id: find(noun_words, "House"),
+    dictionary_id: nouns
+)
+
+hunger = DictionaryEntry.create(
+    word_id: find(noun_words, "Hunger"),
+    dictionary_id: nouns
+)
+
+river = DictionaryEntry.create(
+    word_id: find(noun_words, "River"),
+    dictionary_id: nouns
+)
+
+rock = DictionaryEntry.create(
+    word_id: find(noun_words, "Rock"),
+    dictionary_id: nouns
+)
+
+ten = DictionaryEntry.create(
+    word_id: find(noun_words, "Ten"),
+    dictionary_id: nouns
+)
+
+tree = DictionaryEntry.create(
+    word_id: find(noun_words, "Tree"),
+    dictionary_id: nouns
+)
+
+
