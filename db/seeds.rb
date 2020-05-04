b7aaf30fa56d214a6dad7b5c1164916fef130f8b
Sydney Werbach <sydwer@gmail.com>
2020-05-03 21:25:38 -0600

    Fixed problem with passing in arrays of part of speech words into find function. This will reduce the amount of searching the find function has to do within creating seeds, as it will cut the objects it's sifting though into much smaller chunks, rather than doing the whole 'Word.all' list it was original functioning in'

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,23 @@ Language.destroy_all
 Dictionary.destroy_all
 DictionaryEntry.destroy_all
 
-nouns_words = ["Apple","Bear","Book", "Cat", "Dinner", "Fish",
+nouns_english = ["Apple","Bear","Book", "Cat", "Dinner", "Fish",
 "Hello", "House","Hunger", "River","Rock", "Ten","Tree"];
 
-pronouns_words = ["I", "You","(Formal) You", "He", "She", "It", "We", "You (Plural)",
+pronouns_english = ["I", "You","(Formal) You", "He", "She", "It", "We", "You (Plural)",
 "Them", "Them (Masculine)", "Them (Feminine)", "Them (Neutral)"]
 
-verbs_words = ["Able","Eat", "Enjoy", "Die", "Give", "Have", "Say",
+verbs_english = ["Able","Eat", "Enjoy", "Die", "Give", "Have", "Say",
 "Copula", "Want","Will"];
 
-tenses_words =["Past","Present", "Future", "Imperfect", "Conditional"];
+tenses_english =["Past","Present", "Future", "Imperfect", "Conditional"];
 
-determiners_words = ["The", "A", "This", "That"];
+determiners_english = ["The", "A", "This", "That"];
 
-prepositions_words = ["By", "Through", "Along", "For", "Against",
+prepositions_english = ["By", "Through", "Along", "For", "Against",
 "Towards", "Without","About","From"];
 
-particles_words = ["Topic","Subject", "Object", "Also", "Toward", "From", "By", "At"]
+particles_english = ["Topic","Subject", "Object", "Also", "Toward", "From", "By", "At"]
 
 # grammar = [nouns, pronouns, verbs, tenses, determiners, prepositions];
 
@@ -33,54 +33,54 @@ languages = ["English", "German", "Japanese"];
 
 
 
-nouns_words.map{
+nouns_words = nouns_english.map{
     |noun| Word.create(
         english: noun,
         category: "noun",
     )
 }
 
-apple_test = Word.create(
-    english: "Apple",
-    category: "noun",
-)
+# apple_test = Word.create(
+#     english: "Apple",
+#     category: "noun",
+# )
 
-verbs_words.map{
+verbs_english.map{
     |verb| Word.create(
         english: verb,
         category: "verb",
     )
 }
 
-tenses_words.map{
+tenses_english.map{
     |tense| Word.create(
         english: tense,
         category: "tense",
     )
 }
 
-determiners_words.map{
+determiners_english.map{
     |determiner| Word.create(
         english: determiner,
         category: "determiner",
     )
 }
 
-pronouns_words.map{
+pronouns_english.map{
     |pronoun| Word.create(
         english: pronoun,
         category: "pronoun",
     )
 }
 
-prepositions_words.map{
+prepositions_english.map{
     |preposition| Word.create(
         english: preposition,
         category: "preposition",
     )
 }
 
-particles_words.map{
+particles_english.map{
     |particle| Word.create(
         english: particle,
         category: "particle",
@@ -186,74 +186,74 @@ particles = Dictionary.create(
     part_of_speech: "particles"
 )
 
-def find(string)
-    # array.find{ |entry| entry.english == string }
-    target = Word.all.find{ |entry| entry.english == string }
+def find(array, string)
+    target = array.find{ |entry| entry.english == string }
+    # target = Word.all.find{ |entry| entry.english == string }
     return target
     
 end
 
 apple = DictionaryEntry.create(
-    word_id: find("Apple").id,
+    word_id: find(nouns_words,"Apple").id,
     dictionary_id: nouns.id
 )
 bear = DictionaryEntry.create(
-    word_id: find("Bear").id,
+    word_id: find(nouns_words,"Bear").id,
     dictionary_id: nouns.id
 )
 
 book = DictionaryEntry.create(
-    word_id: find("Book").id,
+    word_id: find(nouns_words,"Book").id,
     dictionary_id: nouns.id
 )
 
 cat = DictionaryEntry.create(
-    word_id: find("Cat").id,
+    word_id: find(nouns_words,"Cat").id,
     dictionary_id: nouns.id
 )
 
 dinner = DictionaryEntry.create(
-    word_id: find("Dinner").id,
+    word_id: find(nouns_words,"Dinner").id,
     dictionary_id: nouns.id
 )
 
 fish = DictionaryEntry.create(
-    word_id: find("Fish").id,
+    word_id: find(nouns_words,"Fish").id,
     dictionary_id: nouns.id
 )
 
 hello = DictionaryEntry.create(
-    word_id: find("Hello").id,
+    word_id: find(nouns_words,"Hello").id,
     dictionary_id: nouns.id
 )
 
 house = DictionaryEntry.create(
-    word_id: find("House").id,
+    word_id: find(nouns_words,"House").id,
     dictionary_id: nouns.id
 )
 
 hunger = DictionaryEntry.create(
-    word_id: find("Hunger").id,
+    word_id: find(nouns_words,"Hunger").id,
     dictionary_id: nouns.id
 )
 
 river = DictionaryEntry.create(
-    word_id: find("River").id,
+    word_id: find(nouns_words,"River").id,
     dictionary_id: nouns.id
 )
 
 rock = DictionaryEntry.create(
-    word_id: find("Rock").id,
+    word_id: find(nouns_words,"Rock").id,
     dictionary_id: nouns.id
 )
 
 ten = DictionaryEntry.create(
-    word_id: find("Ten").id,
+    word_id: find(nouns_words,"Ten").id,
     dictionary_id: nouns.id
 )
 
 tree = DictionaryEntry.create(
-    word_id: find("Tree").id,
+    word_id: find(nouns_words,"Tree").id,
     dictionary_id: nouns.id
 )
 
