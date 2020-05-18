6ba14f02064c544ecac25b105b7f185e1bc4c7bc
Sydney Werbach <sydwer@gmail.com>
2020-05-17 23:58:57 -0600

    Started seeds for determiners, edited name of dictionary to determiner_words rather than the determiners_english that I had accidently used earlier

--- db/seeds.rb
+++ db/seeds.rb
@@ -397,3 +397,40 @@ non_past = DictionaryEntry.create(
 # determiners_english = ["The", "A", "This", "That", "(Close To Speaker)","(Close To Listener)",
 # "(Far From Listener And Speaker)", "(Which One)"];
 
+the = DictionaryEntry.create(
+    word_id: find(determiners_words, "The"),
+    dictionary_id: determiners
+)
+a = DictionaryEntry.create(
+    word_id: find(determiners_words, "A"),
+    dictionary_id: determiners
+)
+this = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+that = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+close_to_speaker = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+close_to_listener = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+far_from_listener_and_speaker = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+which_one = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+ = DictionaryEntry.create(
+    word_id: find(determiners_words, ""),
+    dictionary_id: determiners
+)
+
